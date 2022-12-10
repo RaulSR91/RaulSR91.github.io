5de8bf0402234e2ce5a153cf35ddffb51d441dee
Raul Salazar Romero <rsalazar@procesar.com>
2022-12-10 04:04:27 -0600

    Implements cache

--- sw.js
+++ sw.js
@@ -1,33 +1,42 @@
 const CACHE_NAME = "PROCESAGRAM_CACHE";
 
-self.addEventListener("install",()=>{
+self.addEventListener("install",(evt)=>{
 
 //El waitUntil es similar al return pero de este evento
   //
+  //El waitUntil es similar al return pero de este evento
   evt.waitUntil(
-    self.caches.open(CACHE_NAME).then((cache) => {
-      //Acá se guarda el cache del service worker
+    (async function install() {
+      let cache = await self.caches.open(CACHE_NAME);
       return cache.addAll([
-        "index.html",
-        "style.css"
-      ]);
-    })
+      "manifest.json",
+      "index.html",
+      "style.css",
+      "/node_modules/bootstrap/dist/css/bootstrap.css",
+      "camera.js"
+    ]);
+    })()
   );
 
 
-    self.skipWaiting();
+   // self.skipWaiting();
 })
 self.addEventListener("activate",(event)=>{
     event.waitUntil(self.clients.claim());
 })
 
-self.addEventListener("fetch",()=>{
+self.addEventListener("fetch",(evt)=>{
      //caso 1 : Si estoy offline devuelvo el index del cache
-  if (!navigator.onLine && evt.request.url.includes("index.html")) {
+  if (!navigator.onLine && evt.request.url.includes("index.html")||
+      !navigator.onLine && evt.request.url.includes("style.css")||
+      !navigator.onLine && evt.request.url.includes("bootstrap.css")||
+      !navigator.onLine && evt.request.url.includes("manifest.json")||
+      !navigator.onLine && evt.request.url.includes("camera.js")
+  ) {
     evt.respondWith(
       (async () => {
         console.log("SW", "Fetch", evt.request.url);
-        var cache = await self.caches.open(CACHE);
+        var cache = await self.caches.open(CACHE_NAME);
         var match = await cache.match(evt.request);
         if (match) return match;
       })()
