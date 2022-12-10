8fd895243c9b3b3cfac7a49139ae2a547a624168
Raul Salazar Romero <rsalazar@procesar.com>
2022-12-10 05:34:54 -0600

    Implements maps

---
+++ map.js
@@ -0,0 +1,43 @@
+let map;
+
+let latitude = 19.337831425890258;
+let longitude = -99.19726553942392;
+
+
+// Attach your callback function to the `window` object
+window.initMap = function () {
+  
+};
+
+
+function loadScript() {
+    var script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCXN9mr1uoMr26uC_pPuXEmoNjXAwnB7eo&callback=initialize';
+    script.async = true;
+    document.body.appendChild(script);
+}
+
+window.onload = loadScript;
+
+
+function initialize() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: latitude,
+            lng: longitude
+        },
+        zoom: 18,
+    });
+
+    var marker = new google.maps.Marker({
+        position: {
+            lat: latitude,
+            lng: longitude
+        },
+        title: "ALTTO - San Ángel"
+    });
+
+    marker.setMap(map);
+}
+
+window.initMap = initMap;
